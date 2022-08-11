5de59b0f555b31fadce960da2800dc01e54c17ad
jaldabaoth-code <grialazurabi@gmail.com>
2022-08-11 08:57:27 +0200

    save

--- angular/src/app/cda-workshops/calculator2/calculator2.component.ts
+++ angular/src/app/cda-workshops/calculator2/calculator2.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, HostListener, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-calculator2',
@@ -7,8 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Calculator2Component implements OnInit {
 
-  operator: string;
-  operationsSave: string;
+  operationsSave: string ='';
   sum: number = 0;
 
   constructor() { }
@@ -19,33 +18,52 @@ export class Calculator2Component implements OnInit {
 
       if (this.operationsSave) {
           let lastOperation = this.operationsSave.slice(this.operationsSave.length - 1);
-
-          //this.operationsSave += number;
           if (number == '-' || number == '+' || number == '/' || number == '*') {
               if (lastOperation == '-' || lastOperation == '+' || lastOperation == '/' || lastOperation == '*') {
-                this.operationsSave = this.operationsSave.slice(0, -1) + number + '';
-              } else {
-                this.operationsSave = this.operationsSave + number;
+                  this.operationsSave = this.operationsSave.slice(0, -1);
               }
           } else {
-            this.operationsSave = this.operationsSave + number;
+            if (lastOperation == '-' || lastOperation == '+' || lastOperation == '/' || lastOperation == '*') {
+              //this.operationsSave = this.operationsSave + number;
+            } else {
+              if (this.sum !=0) {
+              return;}
+            }
           }
+          this.operationsSave = this.operationsSave + number;
       } else {
-        this.operationsSave = number;
+          this.operationsSave = number;
       }
 
   }
 
 
-  cancel() {
-    // Cancel all values
-    this.sum = 0;
-    this.operationsSave = '';
-  }
+    cancel() {
+        // Cancel all values
+        this.sum = 0;
+        this.operationsSave = '';
+    }
 
-  equal() {
-    //this.sum = +this.operationsSave;
-    this.sum = eval(this.operationsSave);
-    this.cancel();
+    equal() {
+        this.sum = eval(this.operationsSave);
+        this.operationsSave = this.sum + '';
+    }
+
+
+  numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '+', '-', '*', '/'];
+  counter = 0;
+  @HostListener('window:keydown' , ['$event'])
+  handleKeyDown(event: KeyboardEvent) {
+    if (this.numbers.indexOf(event.key) !== -1) {
+    //if (event.key === '1' || ) {
+      this.operationsSave += event.key;
+    }
+    if (event.key === 'Enter') {
+      this.equal();
+    }
+
   }
+/*  resetCounter() {
+    this.counter = 0;
+  }*/
 }
